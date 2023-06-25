480cc592a6a5330323b2db52cc7bec13a4a7e8de
HalParadis <halparadis@gmail.com>
2023-06-24 23:09:50 -0700

    finished db/init_db buildTables - untested

--- db/init_db.js
+++ db/init_db.js
@@ -9,10 +9,47 @@ async function buildTables() {
     client.connect();
 
     // drop tables in correct order
+    console.log('Starting to drop tables...');
+
+    await client.query(`
+      DROP TABLE IF EXISTS users_items;
+      DROP TABLE IF EXISTS users;
+      DROP TABLE IF EXISTS ramen;
+    `);
+
+    console.log('Finished dropping tables!');
 
     // build tables in correct order
+    console.log('Starting to construct tables...');
+
+    await client.query(`
+      CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        address VARCHAR(255),
+        "isAdmin" BOOLEAN DEFAULT false
+      );
+      CREATE TABLE ramen(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        price VARCHAR(255) NOT NULL,
+        description TEXT,
+        brand VARCHAR(255) NOT NULL,
+        UNIQUE (name, brand)
+      );
+      CREATE TABLE users_items(
+        id SERIAL PRIMARY KEY,
+        "userId" INTEGER REFERENCES users(id),
+        "ramenId" INTEGER REFERENCES ramen(id),
+        count INTEGER
+      );
+    `);
+
+    console.log('Finished constructing tables!');
   } catch (error) {
-    throw error;
+    console.error(error);
   }
 }
 
