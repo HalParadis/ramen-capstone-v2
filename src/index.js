ff19731d9d396eb75f5bf800c07c77847ef96ab5
HalParadis <halparadis@gmail.com>
2023-06-28 01:54:28 -0700

    working on solving react-dom/client error

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
+// import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOMClient from 'react-dom/client';
 import { App } from './components';
 // css stylesheets can be created for each component
 // place them in the src/style directory, and import them like this:
 import './style/index.css';
 
-// const rootEl = document.getElementById('app');
-// const root = ReactDOMClient.createRoot(rootEl);
-// root.render(<BrowserRouter><App/></BrowserRouter>);
+const rootEl = document.getElementById('app');
+const root = ReactDOMClient.createRoot(rootEl);
+root.render(<BrowserRouter><App/></BrowserRouter>);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
